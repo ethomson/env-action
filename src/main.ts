4da277caca580a954556e2ba28da5b862ebbfaff
Edward Thomson <ethomson@edwardthomson.com>
2021-02-01 23:21:29 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    for (const key of Object.keys(process.env).sort()) {
+      console.log(`${key}=${process.env[key]}`)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
