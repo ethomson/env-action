98c11a86921e1bda49980be0f0bd6a252f0c5053
Edward Thomson <ethomson@edwardthomson.com>
2024-05-06 12:16:33 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     for (const key of Object.keys(process.env).sort()) {
-      console.log(`${key}=${process.env[key]}`)
+      console.log(`${key}: ${process.env[key]}`)
     }
   } catch (error) {
     core.setFailed(error.message)
